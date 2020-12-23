4a71437aa92a3db55b2c8c9597a744b1b7baef3e
Choi Jiwon <winning1993@naver.com>
2020-12-23 18:09:05 +0900

    #15.0 Local Only Fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useReactiveVar } from "@apollo/client";
 import React from "react"
 import {LoggedOutRouter} from "./routers/logged-out-router"
+import {LoggedInRouter} from "./routers/logged-in-router"
+import { isLoggedInVar } from "./apollo";
+
+
 
 function App() {
-  return <LoggedOutRouter/>
+  const isLoggedIn = useReactiveVar(isLoggedInVar)
+  
+  return isLoggedIn ? <LoggedInRouter/> : <LoggedOutRouter/>
   
 }
 
