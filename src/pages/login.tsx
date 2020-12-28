24f742a307a986175aa88ca352a3da83b9e2bd21
Choi Jiwon <winning1993@naver.com>
2020-12-29 02:11:53 +0900

    #15.10 UI Clonning

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FormError } from "../components/form-error"
 import { useForm } from "react-hook-form";
 import { ApolloError, gql, useMutation } from "@apollo/client";
+import nuberLogo from "../images/logo.svg"
 import { LoginMutation, LoginMutationVariables } from "../__generated__/LoginMutation"
 
 const LOGIN_MUTATION = gql`
@@ -43,20 +44,22 @@ export const Login = () => {
             })
         }
     }
-    return <span className="h-screen flex items-center justify-center bg-gray-800">
-        <div className="bg-white w-full max-w-lg pt-10 pb-5 rounded-lg text-center">
-            <h3 className="font-3xl text-2xl text-gray-800">Log In</h3>
-            <form onSubmit={handleSubmit(onSubmit)} className="grid gap-3 mt-5 px-5">
-                <input ref={register({required: "Email is required"})} name="email" required type="email" placeholder="Email" className="mb-3 input"></input>
+    return (<div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+        <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={nuberLogo} className="w-52 mb-10" />
+        <h4 className="w-full font-medium text-left text-3xl mb-5">Welcome back</h4>
+            <form onSubmit={handleSubmit(onSubmit)} className="grid gap-3 mt-5 w-full">
+                <input ref={register({required: "Email is required"})} name="email" required type="email" placeholder="Email" className="input"></input>
                 {errors.email?.message && <FormError errorMessage={errors.email?.message}/>}
                 <input ref={register({required: "Password is required", minLength: 3})} name="password" required type="password" placeholder="Password" className="input"></input>
                 {errors.password?.message && <FormError errorMessage={errors.password?.message}/>}
                 {errors.password?.type === "minLength" && <FormError errorMessage="Password must be more than 3 chars."/>}
-            <button className="btn mt-3">
+            <button className="btn">
                 {loading ? "Loading..." : "Log In"}
             </button>
             {loginMutationResult?.login.error && <FormError errorMessage={loginMutationResult.login.error}/>}
             </form>
-        </div>
-    </span>
+            </div>
+    </div>
+    )
 }
